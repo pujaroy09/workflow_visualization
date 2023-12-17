4ab12122e4b503ce509fc4868e42ddb76a96d880
Puja Roy <09pujaroy@gmail.com>
2023-12-17 10:53:15 +0100

    intractive workflow visualization project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<script>
+import BpmnViewer from 'bpmn-js';
+export default {
+  methods: {
+    funcUpload(event) {
+      // var input = event.target;
+      // var text = "";
+      // var reader = new filereader();
+      // var onload = function(event) {
+      //     text = reader.result;
+      //     console.log(text);
+
+      // };
+
+      // reader.onload = onload;
+      // reader.readastext(input.files[0]);
+      var example = event.target.files[0]
+      console.log(event.target.files[0]);
+
+      var viewer = new BpmnViewer({
+  container: '#canvas'
+});
+
+
+viewer.importXML(example).then(function(result) {
+
+  const { warnings } = result;
+
+  console.log('success !', warnings);
+
+  viewer.get('canvas').zoom('fit-viewport');
+}).catch(function(err) {
+
+  const { warnings, message } = err;
+
+  console.log('something went wrong:', warnings, message);
+});
+    },
+  }
+};
+
+</script>
+
+<template>
+  <input type='file' @change='funcUpload'>
+  <div id = "canvas">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
